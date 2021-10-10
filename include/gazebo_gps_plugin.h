3f38c12118c28e3316e44ddbb1ded4ba9c231448
YongraeKim <kimyr911125@gmail.com>
2021-10-10 19:56:36 +0900

    Change sim starting point to doosan tower

--- include/gazebo_gps_plugin.h
+++ include/gazebo_gps_plugin.h
@@ -93,9 +93,9 @@ private:
   // PX4_HOME_LAT, PX4_HOME_LON, and PX4_HOME_ALT
 
   // Zurich Irchel Park
-  double lat_home = 47.397742 * M_PI / 180.0;  // rad
-  double lon_home = 8.545594 * M_PI / 180.0;   // rad
-  double alt_home = 488.0;                     // meters
+  double lat_home = 37.568667 * M_PI / 180.0;  // rad
+  double lon_home = 127.008665 * M_PI / 180.0;   // rad
+  double alt_home = 21.0;                     // meters
   // Seattle downtown (15 deg declination): 47.592182, -122.316031
   // static const double lat_home = 47.592182 * M_PI / 180;    // rad
   // static const double lon_home = -122.316031 * M_PI / 180;  // rad
